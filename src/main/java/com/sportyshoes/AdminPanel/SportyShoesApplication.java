0e9c410fd783bd02216eef0e4cff82872a7be047
sushantkumar <sushantkumar@localhost>
2023-06-24 13:05:20 +0530

    Done lots of work. Everything is working right now specially MYSQL

--- src/main/java/com/sportyshoes/AdminPanel/SportyShoesApplication.java
+++ src/main/java/com/sportyshoes/AdminPanel/SportyShoesApplication.java
@@ -2,8 +2,10 @@ package com.sportyshoes.AdminPanel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan (basePackages = {"com.sportyshoes.*"})
 public class SportyShoesApplication {
 
 	public static void main(String[] args) {
